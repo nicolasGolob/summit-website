1e0d41e59f15a5f6c462e59f3b57915ef5481058
Nicolas Golob <nicolasgolob@gmail.com>
2021-06-10 11:07:45 +0200

    work on the responsive navigation bar with hamburger

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,33 +1,41 @@
-import React, {useState} from 'react';
-import {Link} from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './NavBar.css';
 
-function NavBar() {
-    const [click, setClick] = useState(false);
-    const handleClick = () =>{setClick(!click)};
-    return (
-        <>
-        <nav className='navbar'>
-            <Link to='/home'  exact className='navbar-logo'>
+const NavBar = () => {
+
+	const [openNav, setOpenNav] = useState(false);
+
+	const handleClick = () => {
+		setOpenNav(!openNav);
+	};
+
+	const closeMobileMenu = () => {
+		setOpenNav(false);
+	};
+
+	return (
+		<nav className="navbar">
+			<Link to='/home'  exact className='navbar-logo'>
                 <img src={require('../assets/image/logo.png').default} width="30" height="30" alt='logo'/>
                 SUMMIT 
             </Link> 
-            <ul className='nav-links'>
-                <li className='nav-item'>
-                    <Link to='/home'  className='nav-link'>Home</Link>
-                </li>
-                <li className='nav-item'>
-                    <Link to='/about'  className='nav-link'>About</Link>
-                </li>
-                <li className='nav-item'>
-                    <Link to='/contact'  className='nav-link'>Contact</Link>
-                </li>
-            </ul>   
-            <div className='hamburger-icon' onClick={handleClick}>
-                <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
-            </div>
-        </nav>
-        </> 
-    )
-}
-export default NavBar
+			<div onClick={handleClick} className="nav-icon">
+                <i className={openNav ? 'fas fa-times' : 'fas fa-bars'}/>
+			</div>
+			<ul className={openNav? 'nav-links active' : 'nav-links'}>
+				<li className="nav-item">
+					<Link to='/home'  className='nav-link' onClick={closeMobileMenu}>Home</Link>
+				</li>
+				<li className="nav-item">
+					<Link to='/about'  className='nav-link' onClick={closeMobileMenu}>About</Link>
+				</li>
+				<li className="nav-item">
+					<Link to='/contact'  className='nav-link' onClick={closeMobileMenu}>Contact</Link>
+				</li>
+			</ul>
+		</nav>
+	);
+};
+
+export default NavBar;

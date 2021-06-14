4214d87a2ed62437670634bc740ae36991ac5cef
Nicolas Golob <nicolasgolob@gmail.com>
2021-06-14 15:54:11 +0200

    responsive css adjustments

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ export default function Footer() {
                 <h3>Don't be afraid to contact us</h3>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto suscipit quaerat mollitia ut consequatur accusamus nesciunt odio ab.</p>
                     <ul className="social-media">
-                        <Link to='//github.com/nicolasGolob' target="_blank"><h4 className="media-icon"><i className="fas fa-pen-square"></i></h4></Link>
+                        {/* <Link to='//github.com/nicolasGolob' target="_blank"><h4 className="media-icon"><i className="fas fa-pen-square"></i></h4></Link> */}
                         <Link to='//www.linkedin.com/in/nicolas-golob-67a898184/' target="_blank"><h4 className="media-icon"><i className="fab fa-linkedin-in"></i></h4></Link>
                         <Link to='//github.com/nicolasGolob' target="_blank" ><h4 className="media-icon"><i className="fab fa-github"></i></h4></Link>
                     </ul>

6be32faa080592e44f842c4064efbe4742025b87
Nicolas Golob <nicolasgolob@gmail.com>
2021-06-20 17:04:33 +0200

    css adjustments

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,10 +3,9 @@ import AboutMain from '../AboutMain';
 import AboutText from '../AboutText';
 import Footer from '../Footer';
 
-
 export default function About() {
     return (
-        <>
+        <>      
           <AboutMain/>
           <AboutText/>
           <Footer/>
